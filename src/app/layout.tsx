37e3ceee1dde1b2fd3e05ecd0073586e00ca8896
NotEnoughCoffee <31608111+NotEnoughCoffee@users.noreply.github.com>
2025-01-14 16:40:54 -0800

    Added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from '@vercel/analytics/next';
 import "./globals.css";
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 import { ReactNode } from "react";
@@ -25,6 +26,7 @@ export default function RootLayout(props: { children: ReactNode }) {
         <AppRouterCacheProvider>
           <ThemeProvider theme={theme}>
             {props.children}
+            <Analytics />
           </ThemeProvider>
         </AppRouterCacheProvider>
       </body>
